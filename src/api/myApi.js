8571fe7b6b5a132fb6eca19f306ce5915de79364
Khalil Shakhi <75076708+shakhi95@users.noreply.github.com>
2022-11-05 21:17:13 +0330

    api url changed

--- src/api/myApi.js
+++ src/api/myApi.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: "https://rest-api-shakhi.herokuapp.com",
+// });
+
 export default axios.create({
-  baseURL: "https://rest-api-shakhi.herokuapp.com",
+  baseURL: "https://hashtag-api.iran.liara.run",
 });
